ab1679a2840ce5737fbb55452dbd4f62370368b8
marhosa2 <haasfx129@gmail.com>
2025-04-19 22:39:10 +0800

    Add backend practice file and did stuff with it

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import {renderOrderSumary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import '../data/backend-practice.js';
 //practice only// import '../data/cart-class.js';
+
 
 renderOrderSumary();
 renderPaymentSummary();
